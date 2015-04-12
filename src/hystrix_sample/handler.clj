952173ef331f1178169b9f53a1f1df4957608787
eunmin <telepopsound@gmail.com>
2015-04-13 00:17:02 +0900

    refactor

--- src/hystrix_sample/handler.clj
+++ src/hystrix_sample/handler.clj
@@ -10,43 +10,56 @@
            [com.netflix.hystrix HystrixCommandMetrics]
            [com.netflix.hystrix HystrixCircuitBreaker$Factory]))
 
+(def circuit-breaker-open? (atom false))
+
 (defcommand hello-cmd
   {:hystrix/command-key :hello-cmd
    :hystrix/fallback-fn (fn [args] "error")}
   [message]
-  (Thread/sleep 2000)
-  (if (> (Math/random) 0.5)
+  (if (not @circuit-breaker-open?)
+    (Thread/sleep (rand-int 2000)))
+  (if (> (Math/random) 0.8)
     (str "hello " message)
     (throw (IllegalStateException.))))
 
-(defn show-status []
-  (let [metrics (HystrixCommandMetrics/getInstance (HystrixCommandKey$Factory/asKey "hello-cmd"))
-        circuit-breaker (if metrics (HystrixCircuitBreaker$Factory/getInstance (.getCommandKey metrics)))]
-    (if circuit-breaker
-      (println (.isOpen circuit-breaker))
-      (println "null"))))
+(defn- get-hystrix-key [cmd-key]
+  (HystrixCommandKey$Factory/asKey (name cmd-key)))
 
-(defroutes app-routes
-  (GET "/" []
-       (show-status)
-       (hello-cmd "world"))
-  (route/not-found "Not Found"))
+(defn- get-metrics [hystrix-cmd-key]
+  (HystrixCommandMetrics/getInstance hystrix-cmd-key))
 
-(defn set-config []
-  (.setProperty (ConfigurationManager/getConfigInstance) "hystrix.command.hello-cmd.circuitBreaker.requestVolumeThreshold" 10)
-;  (.setProperty (ConfigurationManager/getConfigInstance) "hystrix.command.hello-cmd.circuitBreaker.enabled" false)
-  (.setProperty (ConfigurationManager/getConfigInstance) "hystrix.command.hello-cmd.circuitBreaker.errorThresholdPercentage" 50)
-  (.setProperty (ConfigurationManager/getConfigInstance) "hystrix.command.hello-cmd.execution.isolation.thread.timeoutInMilliseconds" 1000))
+(defn- get-circuit-breaker [hystrix-cmd-key]
+  (HystrixCircuitBreaker$Factory/getInstance hystrix-cmd-key))
+
+(defn- set-circuit-breaker-status! []
+  (let [hystrix-cmd-key (get-hystrix-key :hello-cmd)]
+    (if-let [metrics (get-metrics hystrix-cmd-key)]
+      (if-let [cb (get-circuit-breaker hystrix-cmd-key)]
+        (swap! circuit-breaker-open? (fn [_] (.isOpen cb)))))))
+
+(defn- set-hystrix-config! [cmd-key k v]
+  (let [key (format "hystrix.command.%s.%s" (name cmd-key) k)]
+    (.setProperty (ConfigurationManager/getConfigInstance) key v)))
+
+(defn- init-hystrix! []
+  (set-hystrix-config! :hello-cmd "circuitBreaker.requestVolumeThreshold" 5)
+  (set-hystrix-config! :hello-cmd "circuitBreaker.errorThresholdPercentage" 50)
+  (set-hystrix-config! :hello-cmd "execution.isolation.thread.timeoutInMilliseconds" 1000))
 
 (defn init []
-  (println "init")
-  (set-config)
+  (init-hystrix!)
   (HystrixRequestContext/initializeContext))
 
 (defn destroy []
-  (println "destory")
   (when-let [c (HystrixRequestContext/getContextForCurrentThread)]
     (.shutdown c)))
+
+(defroutes app-routes
+  (GET "/" []
+       (set-circuit-breaker-status!)
+       (println "circuit breaker open: " @circuit-breaker-open?)
+       (hello-cmd "world"))
+  (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
